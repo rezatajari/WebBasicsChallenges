111f09d70f5d567b5f5c87d7a7e4f03bf57a5e9e
rezatajari <reza.tajari70@gmail.com>
2025-10-05 17:36:50 +0330

    feat: add initial TriviaBot script with fun facts
    Introduces script.js for TriviaBot, which outputs introductory messages and several JavaScript fun facts to the console.

---
+++ 19_TriviaBot/script.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Codey";
+let botLocation = "the Internet";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "A fun fact about " + favoriteLanguage + " is that it can run both on the client and server side.";
+console.log(codingFact);
+
+codingFact = "Another fun fact: " + favoriteLanguage + " was created in just 10 days!";
+console.log(codingFact);
+
+codingFact = "And one more: Many popular frameworks like React use " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
