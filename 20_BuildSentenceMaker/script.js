5514692a376622bcd9c692024fa9a97247a7f3c0
rezatajari <reza.tajari70@gmail.com>
2025-10-05 17:49:54 +0330

    feat: add script to generate and log two stories
    Introduces script.js which constructs and logs two different stories using variable substitution for adjectives, nouns, verbs, and places.

---
+++ 20_BuildSentenceMaker/script.js
@@ -0,0 +1,25 @@
+let adjective = "fierce";
+let noun = "dragon";
+let verb = "flying";
+let place = "mountain";
+let adjective2 = "fiery";
+let noun2 = "sheep";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun +
+                 " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +
+                 " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "happy";
+noun = "unicorn";
+verb = "dancing";
+place = "rainbow";
+adjective2 = "sparkly";
+noun2 = "cookies";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun +
+                  " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +
+                  " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
