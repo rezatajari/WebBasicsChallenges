dcf0534f80b7ac1cae2f08977d7d86b2e477c7b5
rezatajari <reza.tajari70@gmail.com>
2025-10-08 17:59:10 +0330

    feat: add Celsius to Fahrenheit conversion script
    Introduces a function to convert Celsius to Fahrenheit and logs sample conversions for 0, 100, and 25 degrees Celsius.

---
+++ 28_CelsiusFahrenheit/script.js
@@ -0,0 +1,8 @@
+function convertCtoF(celsiusNumber){
+    let =(celsiusNumber*(9/5))+32;
+    return let;
+}
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(100));
+console.log(convertCtoF(25));
